4c103d67bf4bed5cbdebd4c5c403fd7a644d1eda
2371406255 <2371406255@qq.com>
2021-10-10 12:55:36 +0800

    sh

--- push.sh
+++ push.sh
@@ -43,7 +43,7 @@ echo "正在处理这个添加：$add"
 echo "正在处理这个分支: $branchs"
 echo "正在处理这个压缩包: $tar"
 echo "准备执行以下动作: git add $add && git commit -m $branchs && git push -u origin main"
-sleep 3
+sleep 1
 
 #git branch $branchs && git checkout $branchs && git add $add && git commit -m $branchs && git push -u origin $branchs
 git add $add && git commit -m $branchs && git push -u origin main
